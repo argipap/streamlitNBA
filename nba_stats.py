bd86e118cb74036c50ebef7562251246d1cdfabb
argipap <argipapaefstathiou@gmail.com>
2021-01-23 23:01:23 +0200

    merging player stats for a player that has changed teams in the same year

--- nba_stats.py
+++ nba_stats.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from PIL import Image
+from typing import List, Dict
+
 
 st.title('Kantina Basketball Association')
 
@@ -19,14 +21,48 @@ st.markdown("""### NBA player statistics and yahoo fanpoints for KBA league.
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed([str(year)+"-"+str(year+1) for year in range(2010,2021)])))
 
+
+def get_columns(dataframe: pd.DataFrame) -> List:
+    return [column for column in dataframe.head().columns]
+
+
+def get_aggregate_functions(columns: List) -> Dict:
+    aggregation_functions = {}
+    for column in columns:
+        if column not in ("Player", "Pos", "Age", "Tm"):
+            aggregation_functions[column] = "mean"
+        else:
+            aggregation_functions[column] = "last"
+    return aggregation_functions
+
+
+def get_numeric_columns(columns: List):
+    numeric_columns = columns.copy()
+    for column in ("Player", "Pos", "Age", "Tm"):
+        numeric_columns.remove(column)
+    return numeric_columns
+
+
+def convert_columns_to_numeric(dataframe: pd.DataFrame, columns):
+    for column in columns:
+        dataframe[column] = pd.to_numeric(dataframe[column])
+
+
 @st.cache
-def load_data(year):
+def load_data(year: str):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year).split("-")[1] + "_per_game.html"
     html = pd.read_html(url, header = 0)
     data_frame = html[0]
     raw = data_frame.drop(data_frame[data_frame.Age == 'Age'].index) # Deletes repeating headers in content
     raw = raw.fillna(0)
     playerstats = raw.drop(['Rk'], axis=1)
+    aggregation_functions = {}
+    columns = get_columns(playerstats)
+    convert_columns_to_numeric(playerstats, get_numeric_columns(columns))
+    print(playerstats)
+    aggregation_functions = get_aggregate_functions(columns)
+    print(aggregation_functions)
+    playerstats = playerstats.groupby(playerstats['Player']).aggregate(aggregation_functions)
     playerstats["FanPoints"] = playerstats.apply(
         lambda row:
             (float(row['FGA']) * -0.9) +
@@ -44,7 +80,9 @@ def load_data(year):
     )
     playerstats.sort_values(["FanPoints"], ascending=False, inplace=True)
     return playerstats
+
 playerstats = load_data(selected_year)
+print(playerstats)
 
 # Sidebar - Team selection
 sorted_unique_team = sorted(playerstats.Tm.unique())
